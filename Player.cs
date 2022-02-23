64fa8a89440a687b316bd44000a27f294c8bdc6f
shreevyas <shreevyas0211@gmail.com>
2022-02-23 19:25:34 +0530

    Option Check for Player

--- Player.cs
+++ Player.cs
@@ -8,21 +8,47 @@ namespace SnakeAndLadderGame
 {
     internal class Player
     {
-       public int position = 0;
+        public int position = 0;
         public static Random random = new Random();
-        public void move(int roll)
+        public int roll;
+        public void Move(int movement)
         {
-            position += roll;
+            position += movement;
         }
         public void RollDice()
         {
-            int roll = random.Next(1, 7);
-            move(roll);
+            roll = random.Next(1, 7);
+            Move(roll);
+            Option();
         }
-
         public void Display()
         {
             Console.WriteLine("Player position: " + position);
+        }
+
+        public void Option()
+        {
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                    case 0:
+                    Console.WriteLine("No Play");
+                    break;
+
+                case 1:
+                    Console.WriteLine("You are on Snake");
+                    Move(-roll);
+                    break;
+
+                case 2:
+                    Console.WriteLine("You found a ladder");
+                    Move(roll);
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
         }
     }
 }
